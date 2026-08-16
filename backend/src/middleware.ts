3daba3d14c3eac5b664ebbee8bfb313983fa1508
unknown <priyanshulamkhade@gmail.com>
2026-08-16 13:11:17 +0530

    supabase prismadb 2way sync done

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,19 +1,32 @@
 import type { NextFunction, Request, Response } from "express";
 import { createSupabaseClient } from "./lib/client";
+import { prisma } from "../db";
 const client = createSupabaseClient();
 
 
-export async function middleware(req:Request,res:Response,next:NextFunction){
+export async function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization;
     const data = await client.auth.getUser(token);
     const userId = data.data.user?.id
 
-    if(userId){
+    if (userId) {
+        try {
+            await prisma.user.create({
+                data: {
+                    id:data.data.user?.id,
+                    supabaseId:data.data.user?.id!,
+                    email: data.data.user?.email!,
+                    provider: data.data.user?.app_metadata.provider == "google" ? "Google" : "Github",
+                    name: data.data.user?.user_metadata.full_name
+                }
+            })
+        } catch (error){}
+
         req.userId = userId;
         next();
-    }else{
+    } else {
         res.status(403).json({
-            message:"Incorrect inputs"
+            message: "Incorrect inputs"
         })
     }
 }
