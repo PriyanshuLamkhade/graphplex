99229d65530017bb2f9531ec58158a7a340b0391
unknown <priyanshulamkhade@gmail.com>
2026-08-21 11:54:33 +0530

    .env changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,9 @@ export const tavily_client = tavily({ apiKey: process.env.TAVIL_API_KEY });
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT =  Number(process.env.PORT) || 3000;
+
 app.get("/conversations",middleware,async(req,res)=>{
     try {
         const userId = req.userId
@@ -254,19 +257,6 @@ app.post("/conversation_ask/follow_up", middleware, async (req, res) => {
 
 })
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-
-    //MAKE SURE USER HAS ENOUGH TOKENS TO MAKE REQUEST
-
-    //CHECK IF WE HAVE WEB SEARCH INDEXED FOR THE QUERY
-
-    //IF NOT MAKE WEB SEARCH 
-    //DO SOME CONTEXT ENGINEERING ON THE PROMPT  + WEB SEARCH RESULTS
-
-    // HIT THE LLM AND STREAM BACK THE RESPONSE TO THE USER
-
-    //ALSO STREAM BACK THE RESOURCES AND THE FOLLOW UP QUESTIONS TO THE USER(WICH WE CAN DO THAT FROM DIFFERENT PARALLEL LLM CALL)
-
